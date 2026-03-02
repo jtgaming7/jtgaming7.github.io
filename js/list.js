729296033585371bbd254a8909f65be48d164cd2
jtgaming7 <149912786+jtgaming7@users.noreply.github.com>
2026-03-02 15:08:36 -0700

    Update list.js

--- js/list.js
+++ js/list.js
@@ -16,13 +16,13 @@ const d = {
 					"hz": "Mobile"
 				},
 			],
-			"name": "Shitty Cold Sweat",
-                       "author": "MP3141",
+			"name": "The Yandere",
+                       "author": "Dorami",
                        "more": "none",
                        "id": 64360108,
                        "pass": "Free to copy",
-                       "percentToQualify": 60,
-                       "verificationVid": "https://www.youtube.com/watch?v=OSlBFtnbiXg",
+                       "percentToQualify": 46,
+                       "verificationVid": "https://www.youtube.com/watch?v=kLlV43vJ1SE",
                        "key": 0
 		},
 		/*=================================================================================*/
@@ -41,6 +41,20 @@ const d = {
 				       "hz": "300hz" 
 			       },
 		       ],
+                       "name": "Quantum Porcessing",
+                       "author": "Riot",
+                       "more": "none",
+                       "id": 63009517,
+                       "pass": "Free to copy",
+                       "percentToQualify": 45,
+                       "verificationVid": "https://www.youtube.com/watch?v=I4IICgiqfbE&t=65s",
+                       "key": 1
+                },
+		/*=================================================================================*/		
+		{
+                       "vids": [
+			    
+		       ],
                        "name": "Shitty Azure Flare",
                        "author": "Magma",
                        "more": "none",
@@ -50,8 +64,6 @@ const d = {
                        "verificationVid": "https://www.youtube.com/watch?v=I4IICgiqfbE&t=65s",
                        "key": 1
                 },
-		/*=================================================================================*/		
-
 
 
 
@@ -66,4 +78,5 @@ const d = {
 };
 
 const list = d.list;const version = d.version;
+
 
