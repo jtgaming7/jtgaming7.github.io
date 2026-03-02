ba036d096fb98fd34455318257f664fd359e5ce7
jtgaming7 <149912786+jtgaming7@users.noreply.github.com>
2026-03-02 16:52:52 -0700

    Update list please.js

--- js/list.js
+++ js/list.js
@@ -1,91 +1,72 @@
 const d = {
-	"list": [
-	/*=================================================================================*/
-		{
-                       "vids": [
-						   {
-						"user": "JT",
-						"link": " https://www.youtube.com/watch?v=kLlV43vJ1SE",
-						"percent": 46
-						"hz": "" 
-							},
-
-                       ],
-
-						"name": "The Yandere",
-                    	"author": "Ben",
-                    	"more": "none",
-						"id": 63009517,
-                    	"pass": "Free to copy",
-                    	"percentToQualify": 46,
-                    	"verificationVid": "https://www.youtube.com/watch?v=kLlV43vJ1SE",
-                    	"key": 0
-			},
-                },
-	/*=================================================================================*/
-		{
-                       "vids": [
-
-                       ],
-						"name": "Yatagarasu",
-	                    "author": "Ben",
-	                    "more": "none",
-	                    "id": 63009517,
-	                    "pass": "Free to copy",
-	                    "percentToQualify": 45,
-	                    "verificationVid": "https://youtu.be/-p8Z_NU2GB0?si=L3Mmwk_ud5uzXOMl",
-	                    "key": 1
-                },
-        /*=================================================================================*/
-			{
-                       "vids": [
-
-                       ],
-                       "name": "Collapse",
-                       "author": "piesy",
-                       "more": "none",
-                       "id": 113256247,
-                       "pass": "",
-                       "percentToQualify": 1,
-                       "verificationVid": "https://youtu.be/TlKxJYDOws4?si=qDP4tWsUpQ9NMFlw"
-                },
-        /*=================================================================================*/
-		{
-                       "vids": [
-			{
-				       "user": "plimbus",
-				       "link": "https://www.youtube.com/watch?v=s6erfR21x2U&feature=youtu.be",
-				       "percent": 91,
-				       "hz": ""
-				},
-                       ],
-                       "name": "Tartarus",
-                       "author": "piesy",
-                       "more": "none",
-                       "id": 59075347,
-                       "pass": "",
-                       "percentToQualify": 1,
-                       "verificationVid": "https://youtu.be/n1_3JED1Jh8?si=jtJkCan3G16guI1C"
-                },
-        /*=================================================================================*/	
-
-
-
-
-	],
-	"version": [
-		1,
-		0,
-		0
-	]
+  "list": [
+    {
+      "vids": [
+        {
+          "user": "JT",
+          "link": "https://www.youtube.com/watch?v=kLlV43vJ1SE",
+          "percent": 46,
+          "hz": ""
+        }
+      ],
+      "name": "The Yandere",
+      "author": "Ben",
+      "more": "none",
+      "id": 63009517,
+      "pass": "Free to copy",
+      "percentToQualify": 46,
+      "verificationVid": "https://www.youtube.com/watch?v=kLlV43vJ1SE"
+    },
+    {
+      "vids": [],
+      "name": "Yatagarasu",
+      "author": "Ben",
+      "more": "none",
+      "id": 63009518,
+      "pass": "Free to copy",
+      "percentToQualify": 45,
+      "verificationVid": "https://youtu.be/-p8Z_NU2GB0?si=L3Mmwk_ud5uzXOMl"
+    },
+    {
+      "vids": [],
+      "name": "Quantum Processing",
+      "author": "Gabe",
+      "more": "none",
+      "id": 63009519,
+      "pass": "Free to copy",
+      "percentToQualify": 49,
+      "verificationVid": "https://youtu.be/IaofML5KTdQ?si=jQ1Ig7Gzq-k7y1ZK"
+    },
+    {
+      "vids": [],
+      "name": "Auditory Breaker",
+      "author": "Andrew",
+      "more": "none",
+      "id": 63009317,
+      "pass": "Free to copy",
+      "percentToQualify": 45,
+      "verificationVid": ""
+    },
+    {
+      "vids": [],
+      "name": "Ouroboros Startpos 2",
+      "author": "JT",
+      "more": "none",
+      "id": 63009520,
+      "pass": "Free to copy",
+      "percentToQualify": 64,
+      "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl"
+    }
+  ],
+  "version": [1, 0, 0]
 };
 
 d.list.forEach((item, index) => {
-    item.key = index;
+  item.key = index;
 });
 
-const list = d.list;const version = d.version;
-
+const list = d.list;
+const version = d.version;
 
 
 
