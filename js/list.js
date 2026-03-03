3769c50badd81d685e19ec3cbf7e2d3bde8e19eb
jtgaming7 <149912786+jtgaming7@users.noreply.github.com>
2026-03-02 17:23:23 -0700

    Update list4.js

--- js/list.js
+++ js/list.js
@@ -12,8 +12,8 @@ const d = {
       "name": "The Yandere",
       "author": "Ben",
       "more": "none",
-      "id": 63009517,
-      "pass": "Free to copy",
+      "id":  54615662,
+      "pass": "NA",
       "percentToQualify": 46,
       "verificationVid": "https://www.youtube.com/watch?v=kLlV43vJ1SE"
     },
@@ -23,7 +23,7 @@ const d = {
       "author": "Ben",
       "more": "none",
       "id": 63009518,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 45,
       "verificationVid": "https://youtu.be/-p8Z_NU2GB0?si=L3Mmwk_ud5uzXOMl"
     },
@@ -33,7 +33,7 @@ const d = {
       "author": "Gabe",
       "more": "none",
       "id": 63009519,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 49,
       "verificationVid": "https://youtu.be/IaofML5KTdQ?si=jQ1Ig7Gzq-k7y1ZK"
     },
@@ -43,7 +43,7 @@ const d = {
       "author": "Andrew",
       "more": "none",
       "id": 63009520,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 45,
       "verificationVid": ""
     },
@@ -53,7 +53,7 @@ const d = {
       "author": "JT",
       "more": "none",
       "id": 63009521,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 64,
       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl"
     },
@@ -63,7 +63,7 @@ const d = {
       "author": "JT",
       "more": "none",
       "id": 63009522,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 51,
       "verificationVid": "https://www.youtube.com/watch?v=bvObypx7roQ&t=1s"
     },
@@ -73,7 +73,7 @@ const d = {
       "author": "Ben",
       "more": "none",
       "id": 63009523,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 54,
       "verificationVid": "https://youtu.be/kS29Kmzw55o?si=NPSuQYsaP-e37Jf6"
     },
@@ -83,7 +83,7 @@ const d = {
       "author": "JT",
       "more": "none",
       "id": 63009524,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 55,
       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl"
     },
@@ -93,7 +93,7 @@ const d = {
       "author": "Ben",
       "more": "none",
       "id": 63009525,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 52,
       "verificationVid": "https://youtu.be/KrCrLQz05KA?si=uMGyR-54Bhy0zp3r"
     },
@@ -103,7 +103,7 @@ const d = {
       "author": "JT",
       "more": "none",
       "id": 63009526,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 52,
       "verificationVid": "https://youtu.be/2ImqD2LMlqs?si=60LCz3xXHz3ZM6Y8"
     },
@@ -132,7 +132,7 @@ const d = {
       "author": "Gabe",
       "more": "none",
       "id": 63009527,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 42,
       "verificationVid": "https://youtu.be/L2aKNNaGkRY"
     },
@@ -142,10 +142,27 @@ const d = {
       "author": "Andrew",
       "more": "none",
       "id": 63009528,
-      "pass": "Free to copy",
+      "pass": "NA",
       "percentToQualify": 47,
       "verificationVid": ""
+    },
+    "vids": [ 
+            {
+          "user": "Ben",
+          "link": "https://youtu.be/weDbuw-G330?si=D3uuu8dZWpvTALNH",
+          "percent": 100,
+          "hz": "240hz"
+        }
+    ],
+      "name": "Empyrean",
+      "author": "JT",
+      "more": "none",
+      "id": 63009528,
+      "pass": "NA",
+      "percentToQualify": 57,
+      "verificationVid": "https://youtu.be/bbhrc1ueYHo?si=H8zZ_jvJb2zHKfeM"
     }
+    
   ],
   "version": [1, 0, 0]
 };
@@ -156,6 +173,7 @@ d.list.forEach((item, index) => {
 
 const list = d.list;
 const version = d.version;
+
 
 
 
