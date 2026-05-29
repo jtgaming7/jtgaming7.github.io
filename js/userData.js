afcb831ced69752bdb08ee4df91ef26c734648f1
jtgaming7 <149912786+jtgaming7@users.noreply.github.com>
2026-05-29 16:31:50 -0700

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -111,8 +111,9 @@ function getUserData(user) {
     }
 
     for (var i = 0 ; i < user_data[user].progress.length ; i++) {
-        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 120 ? '' : ' / '+user_data[user].progress[i].hz)+')<strong></a></li>'
-        if (user_data[user].progress[i].progress == 100) {
+        var isLegacy = user_data[user].progress[i].legacy;
+        var entryText = user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% (#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 120 ? '' : ' / '+user_data[user].progress[i].hz)+')';
+        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + (isLegacy ? '<em>'+entryText+'</em>' : entryText) + '</a></li>'        if (user_data[user].progress[i].progress == 100) {
             clears++;
         }
     }
